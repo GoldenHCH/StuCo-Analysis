362ecd4a3c5552b7fb06a95fe615133f488994a7
GoldenHCH <109107564+GoldenHCH@users.noreply.github.com>
2023-08-22 12:07:00 -0600

    Update AI.py

--- AI.py
+++ AI.py
@@ -1,20 +1,16 @@
-import os
-import openai
+money = 5000
+# month = 0
 
-openai.api_key = "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"
+# while money < 150000:
+  # money = money*1.05+450
+  # month = month+1
 
-# paragraph = "We planned the event in the classroom. It was a very stressful process, but we completed the script. The theme was gold, and we decorated with gold stars and streamers. The event itself was really fun and everything went smoothly for the most part."
+for month in range(0,12):
+  money = money*1.05+400
+  month = month+1
 
-def ai(paragraph):
-  response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt= f"extract the keywords and classify the sentiment from {paragraph}",
-    temperature=0.9,
-    max_tokens=1000,
-    top_p=1.0,
-    frequency_penalty=0.8,
-    presence_penalty=0.0
-  )
-  return str(response['choices'][0]['text'])
+print(month)
+print(money)
 
-# print(ai(paragraph))
+loan_amount = 400000
+rent = 2000
